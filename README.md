ec9b106b12a98c99a8b505eca37cf532d1d96792
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-08-26 09:22:14 +0900

    add contact page,modify routing,css

--- README.md
+++ README.md
@@ -9,3 +9,8 @@
     - 詳しいことは[自分のブログ](http://kic-yuuki.hatenablog.com/entry/2018/08/17/183031)参照
 - BootStrapによるレイアウト適用
 - パーシャル（部分ビュー）
+- URL
+    - 意図したURLに変更する場合は以下のような感じ
+    - `get '/free_name' to: 'controller_name/method_name', as: 'free'`
+        - `as`有りなら`free_url,free_path`
+        - `as`無しなら`free_name_url,free_name_path`
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,27 +7,33 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get root" do
-    get root_url
+    get root_path
     assert_response :success
     assert_select "title", "home #{@base_title}"
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get home_path
     assert_response :success
     assert_select "title", "home #{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "help #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "about #{@base_title}"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "contact #{@base_title}"
   end
 
 end
