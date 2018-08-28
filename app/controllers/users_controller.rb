ad493ce6091d498c8039efcd7d5cda9c891af8ce
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-08-29 08:18:49 +0900

    add gravatar, user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def show
     @user = User.find(params[:id])
