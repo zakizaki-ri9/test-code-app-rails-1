fc03906b8e4c15a4d43a6eb54541508c8325d04a
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-09-01 13:02:37 +0900

    add success message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Create success!!"
+      redirect_to @user
     else
       render 'new'
     end
