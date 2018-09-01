993aee89df6c2769f64577ae2d95d9dda3ea2cca
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-09-01 13:24:23 +0900

    add signup valid test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -27,5 +27,34 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
 
   end
+
+  test "valid signup information" do
+
+    # 登録ページへアクセス
+    get signup_path
+
+    # Postでエラーとなる登録情報を送信
+    #  assert_no_differenceでUserのカウント数が変化しない（登録されない）ことを確認
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+        user: {
+          name: "example",
+          email: "example@example.com",
+          password: "example",
+          password_confirmation: "example"
+        }
+      }
+    end
+
+    # POSTリクエスト送信結果からリダイレクト先のURLへ移動する
+    follow_redirect!
+
+    # showのViewが表示されていることを確認
+    assert_template "users/show"
+
+    # flashが設定されていることを確認
+    assert_not flash[:success].nil?
+
+  end
   
 end
