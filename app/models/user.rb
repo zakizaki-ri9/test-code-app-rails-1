3e1813e26f7d9c29a7938af050749f6b194de008
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-09-04 08:53:38 +0900

    user create token method for session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
 
+    # インスタンス変数への読み書き用アクセサを定義
+    attr_accessor :remember_token
+
     # 保存時に小文字変換
     before_save {self.email = email.downcase}
 
@@ -20,9 +23,25 @@ class User < ApplicationRecord
         presence: true,
         length: {minimum: 6}
 
-    # 少ないコストの文字列のハッシュ値を返す
-    def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine::cost
-        BCrypt::Password.create(string, cost: cost)
+    # 永続セッションのためにユーザー情報をDBに記憶
+    def remember
+        # 新たに生成したトークンで更新
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
     end
+
+    class << self
+        # 少ないコストの文字列のハッシュ値を返す
+        def digest(string)
+            cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine::cost
+            BCrypt::Password.create(string, cost: cost)
+        end
+    
+        # ランダムトークン生成
+        def new_token
+            # A-Z,a-z,0-9,-,_の組み合わせで計22文字の文字列を出力
+            SecureRandom.urlsafe_base64
+        end
+    end
+
 end
