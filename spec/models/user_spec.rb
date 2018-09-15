93e865541e43112256cc41dfda4aa8686e6b2734
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-09-15 15:48:33 +0900

    test user data create by factorybot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,49 +2,31 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  let(:nil_user) { User.new(nil_params) }
-  let(:nil_params) {{ name: nil, email: nil, password: nil }}
+  let(:user_nil) { build(:user_nil) }
+  let(:user) { build(:user) }
 
   it '名前が空だったら無効であること' do
-    nil_user.valid?
-    expect(nil_user.errors[:name]).to_not include(nil)
+    user_nil.valid?
+    expect(user_nil.errors[:name]).to_not include(nil)
   end
   it 'メールアドレスが空だったら無効であること' do
-    nil_user.valid?
-    expect(nil_user.errors[:email]).to_not include(nil)
+    user_nil.valid?
+    expect(user_nil.errors[:email]).to_not include(nil)
   end
   it 'メールアドレスに@がなかったら無効であること' do
-    user = User.new(
-      name: 'sample_user',
-      email: 'sample_user+ex.com',
-      password: 'sample_user'
-    )
+    user.email = 'example_user+ex.com'
     user.valid?
     expect(user.errors[:email]).to_not include(nil)
   end
   it ' メールアドレスが重複していたら無効であること' do
-    User.create(
-      name: 'sample_user',
-      email: 'sample_user@ex.com',
-      password: 'sample_user'
-    )
-    user = User.new(
-      name: 'sample_user2',
-      email: 'sample_user@ex.com',
-      password: 'sample_user2'
-    )
+    build(:user).save
     expect(user).to_not be_valid
   end
   it 'パスワードが空だったら無効であること' do
-    nil_user.valid?
-    expect(nil_user.errors[:password]).to_not include(nil)
+    user_nil.valid?
+    expect(user_nil.errors[:password]).to_not include(nil)
   end
   it '正常であること' do
-    user = User.new(
-      name: 'sample_user',
-      email: 'sample_user@ex.com',
-      password: 'sample_user'
-    )
     expect(user).to be_valid
   end
 end
